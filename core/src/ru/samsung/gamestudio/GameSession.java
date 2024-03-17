ee21b90d66c10bc00e5d29666b62acdb96be9083
glebpav <glebpavlyuk@gmail.com>
2024-03-18 01:26:14 +0300

    refactoring ui and pause timer

--- core/src/ru/samsung/gamestudio/GameSession.java
+++ core/src/ru/samsung/gamestudio/GameSession.java
@@ -8,6 +8,7 @@ public class GameSession {
     public GameState state;
     long nextEnemySpawnTime;
     long sessionStartTime;
+    long pauseStartTime;
 
     public GameSession() {
     }
@@ -21,11 +22,14 @@ public class GameSession {
 
     public void pauseGame() {
         state = GameState.PAUSED;
+        pauseStartTime = TimeUtils.millis();
     }
 
     public void resumeGame() {
         state = GameState.PLAYING;
+        sessionStartTime += TimeUtils.millis() - pauseStartTime;
     }
+
 
     public boolean shouldSpawnTrash() {
         if (nextEnemySpawnTime <= TimeUtils.millis()) {
