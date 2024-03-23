d12a7e12bdec000b56f7265a6bcd9faf8eb537f1
glebpav <glebpavlyuk@gmail.com>
2024-03-23 22:57:31 +0300

    rewrite objects initialization

--- core/src/ru/samsung/gamestudio/objects/BulletObject.java
+++ core/src/ru/samsung/gamestudio/objects/BulletObject.java
@@ -8,8 +8,8 @@ public class BulletObject extends GameObject {
 
     public boolean wasHit;
 
-    public BulletObject(float x, float y, World world) {
-        super("textures/bullet.png", x, y, 12, 44, GameSettings.BULLET_BIT, world);
+    public BulletObject(int x, int y, int width, int height, String texturePath, World world) {
+        super(texturePath, x, y, width, height, GameSettings.BULLET_BIT, world);
         body.setLinearVelocity(new Vector2(0, GameSettings.BULLET_VELOCITY));
         body.setBullet(true);
         wasHit = false;
