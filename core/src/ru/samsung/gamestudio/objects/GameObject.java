a3d3dcaa051c74a98d1d762887d4b3c4aa55c68d
glebpav <glebpavlyuk@gmail.com>
2024-04-06 17:57:42 +0300

    fix GameObject

--- core/src/ru/samsung/gamestudio/objects/GameObject.java
+++ core/src/ru/samsung/gamestudio/objects/GameObject.java
@@ -23,15 +23,11 @@ public class GameObject {
         this.height = height;
 
         texture = new Texture(texturePath);
-        body = createBody(x * SCALE, y * SCALE, world);
+        body = createBody(x, y, world);
     }
 
     public void draw(SpriteBatch batch) {
-        batch.draw(texture,
-                body.getPosition().x / SCALE - (width / 2f),
-                body.getPosition().y / SCALE - (height / 2f),
-                width,
-                height);
+        batch.draw(texture, getX() - (width / 2f), getY() - (height / 2f), width, height);
     }
 
     public int getX() {
@@ -67,7 +63,7 @@ public class GameObject {
         body.createFixture(fixtureDef);
         circleShape.dispose();
 
-        body.setTransform(x, y, 0);
+        body.setTransform(x * SCALE, y * SCALE, 0);
         return body;
     }
 
