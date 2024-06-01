7148f6f17c647d1c5d9954cc3d1f0b2b242ddd32
hlebnoe_pole <glebpavlyuk@gmail.com>
2024-06-01 13:52:31 +0300

    refactor enemy -> trash

--- core/src/ru/samsung/gamestudio/GameSession.java
+++ core/src/ru/samsung/gamestudio/GameSession.java
@@ -1,13 +1,10 @@
 package ru.samsung.gamestudio;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.utils.TimeUtils;
-
-import java.util.ArrayList;
 
 public class GameSession {
 
-    long nextEnemySpawnTime;
+    long nextTrashSpawnTime;
     long sessionStartTime;
 
     public GameSession() {
@@ -15,20 +12,20 @@ public class GameSession {
 
     public void startGame() {
         sessionStartTime = TimeUtils.millis();
-        nextEnemySpawnTime = sessionStartTime + (long) (GameSettings.STARTING_ENEMY_APPEARANCE_COOL_DOWN
-                * getEnemyPeriodCoolDown());
+        nextTrashSpawnTime = sessionStartTime + (long) (GameSettings.STARTING_TRASH_APPEARANCE_COOL_DOWN
+                * getTrashPeriodCoolDown());
     }
 
     public boolean shouldSpawnTrash() {
-        if (nextEnemySpawnTime <= TimeUtils.millis()) {
-            nextEnemySpawnTime = TimeUtils.millis() + (long) (GameSettings.STARTING_ENEMY_APPEARANCE_COOL_DOWN
-                    * getEnemyPeriodCoolDown());
+        if (nextTrashSpawnTime <= TimeUtils.millis()) {
+            nextTrashSpawnTime = TimeUtils.millis() + (long) (GameSettings.STARTING_TRASH_APPEARANCE_COOL_DOWN
+                    * getTrashPeriodCoolDown());
             return true;
         }
         return false;
     }
 
-    private float getEnemyPeriodCoolDown() {
+    private float getTrashPeriodCoolDown() {
         return (float) Math.exp(-0.001 * (TimeUtils.millis() - sessionStartTime + 1) / 1000);
     }
 }
